6ef06a0e5e3bbe48f76382b18aa6610d0b1b3f6e
Ben Leivian <ben@drawbackwards.com>
2017-03-09 01:16:52 -0700

    Add currency formatting to amount.

--- app/Grant.php
+++ app/Grant.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Grant extends Model
 {
-    //
+    protected $fillable = [
+      'description',
+      'amount',
+      'status',
+    ];
+
+
+    public function getAmountAttribute($amount)
+    {
+        return $this->attributes['amount'] = sprintf('$%s', number_format($amount, 2));
+    }
 }
