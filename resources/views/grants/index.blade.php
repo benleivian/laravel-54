f4c402227fd1975ecf7064784e1387b5e71e6c3f
Ben Leivian <ben@drawbackwards.com>
2017-03-11 01:36:11 -0700

    Add remaining amount & button logic.

--- resources/views/grants/index.blade.php
+++ resources/views/grants/index.blade.php
@@ -9,7 +9,12 @@
         {{-- Available Grants --}}
         <div class="panel panel-default">
           <div class="panel-heading">
-            <h2>Grants</h2>
+            <div style="display:flex; align-items:baseline;">
+              <h2>Grants</h2>
+              <h4 style="margin-left:1rem;color:{{ $remaining_amount < 1 ? '#a94442;' : '#0A8' }}">
+                ( @currency( $remaining_amount ) remaining )
+              </h4>
+            </div>
           </div>
           <table class="table">
             <thead>
@@ -31,16 +36,19 @@
             <tfoot>
               <tr>
                 <th>&nbsp;</th>
-                <th>@currency( $grants_total )</th>
-                <th>&nbsp;</th>
+                <th colspan="2">@currency( $grants_total )</th>
               </tr>
             </tfoot>
           </table>
-          <div class="panel-footer">
-            <div class="btn-group" role="group" aria-label="...">
-              <a href="{{ route('grants.create') }}" role="button" class="btn btn-default">Add New Grant</a>
+            <div class="panel-footer">
+              <div class="btn-group" role="group" aria-label="...">
+                @if ( $remaining_amount > 0 )
+                  <a href="{{ route('grants.create') }}" role="button" class="btn btn-default">Add New Grant</a>
+                @else
+                  <span role="button" class="btn btn-disabled" disabled>Additional Funds Needed For New Grants<span>
+                @endif
+              </div>
             </div>
-          </div>
         </div>
       </div>
     </div>
